fbb48cbcfbc2ca2cc4852c6f6bcb4f428529d19e
vivek <vivek@OliveToo.local>
2012-11-19 14:30:13 -0800

    Filled out the Anchor and Record datums.

--- src/main/java/com/scaleunlimited/atomizer/datum/AnchorDatum.java
+++ src/main/java/com/scaleunlimited/atomizer/datum/AnchorDatum.java
@@ -1,14 +1,89 @@
 package com.scaleunlimited.atomizer.datum;
 
 import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
 
 import com.scaleunlimited.cascading.BaseDatum;
 
-// datasetId (String), recordUuid (String), anchorId (String), attribute name (String), atom (String), confidence level (float)
-
+@SuppressWarnings("serial")
 public class AnchorDatum extends BaseDatum {
 
+    public static final String DATASET_ID_FN = BaseDatum.fieldName(AnchorDatum.class, "datasetId");
+    public static final String RECORD_UUID_FN = BaseDatum.fieldName(AnchorDatum.class, "recordUuid");
+    public static final String ANCHOR_ID_FN = BaseDatum.fieldName(AnchorDatum.class, "anchorId");
+    public static final String ATTRIBUTE_NAME_FN = BaseDatum.fieldName(AnchorDatum.class, "attributeName");
     public static final String ATOM_FN = BaseDatum.fieldName(AnchorDatum.class, "atom");
+    public static final String CONFIDENCE_LEVEL_FN = BaseDatum.fieldName(AnchorDatum.class, "confidenceLevel");
+
+    public static final Fields FIELDS = new Fields(
+                    DATASET_ID_FN,
+                    RECORD_UUID_FN,
+                    ANCHOR_ID_FN,
+                    ATTRIBUTE_NAME_FN,
+                    ATOM_FN,
+                    CONFIDENCE_LEVEL_FN);
     
-    public static final Fields FIELDS = new Fields(ATOM_FN);
+    public AnchorDatum(String datasetId, String recordUuid, String anchorId, String attributeName, 
+                    String atom, float confidenceLevel) {
+        super(FIELDS);
+        setDatasetId(datasetId);
+        setRecordUuid(recordUuid);
+        setAnchorId(anchorId);
+        setAttributeName(attributeName);
+        setAtom(atom);
+        setConfidenceLevel(confidenceLevel);
+    }
+    
+    public AnchorDatum(Tuple t) {
+        super(FIELDS, t);
+    }
+
+    public String getDatasetId() {
+        return _tupleEntry.getString(DATASET_ID_FN);
+    }
+    
+    public void setDatasetId(String datasetId) {
+        _tupleEntry.set(DATASET_ID_FN, datasetId);
+    }
+
+    public String getRecordUuid() {
+        return _tupleEntry.getString(RECORD_UUID_FN);
+    }
+    
+    public void setRecordUuid(String recordUuid) {
+        _tupleEntry.set(RECORD_UUID_FN, recordUuid);
+    }
+    
+    public String getAnchorId() {
+        return _tupleEntry.getString(ANCHOR_ID_FN);
+    }
+    
+    public void setAnchorId(String anchorId) {
+        _tupleEntry.set(ANCHOR_ID_FN, anchorId);
+    }
+    
+    public String getAttributeName() {
+        return _tupleEntry.getString(ATTRIBUTE_NAME_FN);
+    }
+    
+    public void setAttributeName(String attributeName) {
+        _tupleEntry.set(ATTRIBUTE_NAME_FN, attributeName);
+    }
+    
+    public String getAtom() {
+        return _tupleEntry.getString(ATOM_FN);
+    }
+    
+    public void setAtom(String atom) {
+        _tupleEntry.set(ATOM_FN, atom);
+    }
+
+    public float getConfidenceLevel() {
+        return _tupleEntry.getFloat(CONFIDENCE_LEVEL_FN);
+    }
+    
+    public void setConfidenceLevel(float confidenceLevel) {
+        _tupleEntry.set(CONFIDENCE_LEVEL_FN, confidenceLevel);
+    }
+
 }
--- src/main/java/com/scaleunlimited/atomizer/datum/RecordDatum.java
+++ src/main/java/com/scaleunlimited/atomizer/datum/RecordDatum.java
@@ -1,9 +1,85 @@
 package com.scaleunlimited.atomizer.datum;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+
 import com.scaleunlimited.cascading.BaseDatum;
 
 // record has datasetId, recordUuid, and a key/value (attribute name/value) map
 
+@SuppressWarnings("serial")
 public class RecordDatum extends BaseDatum {
+
+    public static final String DATASET_ID_FN = BaseDatum.fieldName(RecordDatum.class, "datasetId");
+    public static final String RECORD_UUID_FN = BaseDatum.fieldName(RecordDatum.class, "recordUuid");
+    public static final String ATTRIBUTE_MAP_FN = BaseDatum.fieldName(RecordDatum.class, "attributeMap");
+
+    public static final Fields FIELDS = new Fields(
+                    DATASET_ID_FN,
+                    RECORD_UUID_FN,
+                    ATTRIBUTE_MAP_FN);
+    
+    public RecordDatum(String datasetId, String recordUuid, Map<String, String>attributeMap) {
+        super(FIELDS);
+        setDatasetId(datasetId);
+        setRecordUuid(recordUuid);
+        setAttributeMap(attributeMap);
+    }
+    
+    public RecordDatum(Tuple t) {
+        super(FIELDS, t);
+    }
+
+    public String getDatasetId() {
+        return _tupleEntry.getString(DATASET_ID_FN);
+    }
+    
+    public void setDatasetId(String datasetId) {
+        _tupleEntry.set(DATASET_ID_FN, datasetId);
+    }
+
+    public String getRecordUuid() {
+        return _tupleEntry.getString(RECORD_UUID_FN);
+    }
+    
+    public void setRecordUuid(String recordUuid) {
+        _tupleEntry.set(RECORD_UUID_FN, recordUuid);
+    }
+    
+    public Map<String,String> getAttributeMap() {
+        return tupleToMap((Tuple)(_tupleEntry.getObject(ATTRIBUTE_MAP_FN)));
+    }
+    
+    public void setAttributeMap(Map<String, String> attributeMap) {
+        _tupleEntry.set(ATTRIBUTE_MAP_FN, mapToTuple(attributeMap));
+    }
+
+    private static Tuple mapToTuple(Map<String, ?> map) {
+        if (map == null) {
+            return null;
+        }
+        
+        Tuple result = new Tuple();
+        for (Map.Entry<String,?> entry : map.entrySet()) {
+            result.add(entry.getKey());
+            result.add(entry.getValue());
+        }
+        return result;
+    }
+
+    private Map<String, String> tupleToMap(Tuple tuple) {
+        Map<String, String> result = new HashMap<String, String>();
+        if (tuple != null) {
+            int numHeaders = tuple.size() / 2;
+            for (int i = 0; i < numHeaders; i++) {
+                result.put(tuple.getString(i*2), tuple.getString((i*2)+1));
+            }
+        }
+        
+        return result;
+    }
 
 }
